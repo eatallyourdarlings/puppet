c27479333651111e8988db8cba6b446dd57e50f7
eatallyourdarlings <eatallyourdarlings@gmail.com>
2024-09-21 21:38:36 +1000

    add show_spectrum and test .wav

--- main.gd
+++ main.gd
@@ -1,24 +1,24 @@
 extends Node2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var stream: AudioStreamPlayer = $AudioStreamPlayer
+@onready var mic: AudioStreamPlayer = $Microphone
 var spectrum : AudioEffectSpectrumAnalyzerInstance
 
-const FREQ_MAX = 11050.0 #hz
+#hz
+const FREQ_MAX = 11050.0 
 const FREQ_MIN = 0
 
 func _ready() -> void: 
 	spectrum = AudioServer.get_bus_effect_instance(1,0)
-	stream.play()
-	stream.get_stream_playback()
-	sprite.set_frame(0)
+	#mic.play()
+	#mic.get_stream_playback()
+	sprite.set_frame(3)
 	
 	
-func _process(_delta) -> void:
-	var magnitude : float = spectrum.get_magnitude_for_frequency_range(FREQ_MIN, FREQ_MAX).normalized()
-	#
-	print(magnitude)
-	if magnitude > 0.5:
-		sprite.set_frame(1)
-	else:
-		sprite.set_frame(2)
+#func _process(_delta) -> void:
+	#var magnitude : float = spectrum.get_magnitude_for_frequency_range(FREQ_MIN, FREQ_MAX).normalized().length()
+	#print(magnitude)
+	#if magnitude > 0.5:
+		#sprite.set_frame(1)
+	#else:
+		#sprite.set_frame(2)
